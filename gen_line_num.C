d5cf6b32426e68fcfee96657cecb90c8496852e3
Shivansh Rai <shivansh@freebsd.org>
2017-09-13 22:38:32 +0530

    Minor refactoring

--- gen_line_num.C
+++ gen_line_num.C
@@ -1,3 +1,4 @@
+// Generates line number of all the if statements in the input file
 #include "rose.h"
 
 using namespace std;
@@ -5,21 +6,16 @@ using namespace std;
 int main( int argc, char * argv[] )
 {
     ROSE_INITIALIZE;
-
     SgProject* project = frontend(argc,argv);
     ROSE_ASSERT(project != NULL);
-
     Rose_STL_Container<SgNode*> ifStmtList = NodeQuery::querySubTree (project,V_SgIfStmt);
 
-    for (Rose_STL_Container<SgNode*>::iterator i = ifStmtList.begin(); i != ifStmtList.end(); i++)
-    {
+    for (Rose_STL_Container<SgNode*>::iterator i = ifStmtList.begin(); i != ifStmtList.end(); i++) {
         SgIfStmt *ifStmt = isSgIfStmt(*i);
         ROSE_ASSERT(ifStmt != NULL);
 
         if ( (*i)->get_file_info()->isCompilerGenerated() == false)
-        {
             printf ("%d\n", ifStmt->get_file_info()->get_line());
-        }
     }
     return 0;
 }
